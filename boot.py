918b2a68d98810971611a8b806c8c18c67cc54c7
Karsten Horsmann <khorsmann@gmail.com>
2018-06-24 20:04:39 +0200

    add example files, enable webrepl

--- boot.py
+++ boot.py
@@ -1,15 +1,16 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 #import esp
 #esp.osdebug(None)
-#import webrepl
-#webrepl.start()
+
+def start_webrepl():
+    import webrepl
+    webrepl.start()
 
 def read_config():
     import json
     with open('config.json', 'r')  as f:
         cfg = json.load(f)
     return cfg
-
 
 def connect(essid, password):
     import network
@@ -27,8 +28,19 @@ def no_debug():
     # this can be run from the REPL as well
     esp.osdebug(None)
 
+
 cfg = read_config()
+
+# debug on/off
+if cfg['os']['debug'] is False:
+    no_debug()
+
+# network an webrepl - ws port 8266
 if cfg['network']['enable']:
     print('configure network')
+    connect(cfg['network']['essid'], cfg['network']['password'])
+    if cfg['webrepl']['enable']:
+        print('try to start webrepl on ws://0.0.0.0:8266/')
+        start_webrepl()
 else:
     print('config.json network not enabled')
