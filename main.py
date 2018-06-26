f418ffe78808dd85af875dcf02a9bd37c8263dc8
Karsten Horsmann <khorsmann@gmail.com>
2018-06-26 20:33:59 +0200

    slower and not so bright

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ np = neopixel.NeoPixel(machine.Pin(25), 12)
 
 def clear(np):
     n = np.n
-
     for i in range(n):
         np[i] = (0, 0, 0)
     np.write()
@@ -17,20 +16,20 @@ def demo(np):
     for i in range(4 * n):
         for j in range(n):
             np[j] = (0, 0, 0)
-        np[i % n] = (255, 255, 255)
+        np[i % n] = (1, 1, 1)
         np.write()
         time.sleep_ms(25)
 
     # bounce
     for i in range(4 * n):
         for j in range(n):
-            np[j] = (0, 0, 128)
+            np[j] = (0, 0, 64)
         if (i // n) % 2 == 0:
             np[i % n] = (0, 0, 0)
         else:
             np[n - 1 - (i % n)] = (0, 0, 0)
         np.write()
-        time.sleep_ms(60)
+        time.sleep_ms(120)
 
     # fade in/out
     for i in range(0, 4 * 256, 8):
@@ -49,7 +48,5 @@ try:
         demo(np)
 except KeyboardInterrupt:
     print("quit the demo")
-
 finally:
     clear(np)
-
