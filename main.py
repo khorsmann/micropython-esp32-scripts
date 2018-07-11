b24c5ab11e07d927067eec9fbb89fa1326c43236
Karsten Horsmann <khorsmann@gmail.com>
2018-07-11 17:07:47 +0200

    add disco effect

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import machine, neopixel
 from machine import Pin
 import time
+import random
 
 # NeoPixel on Pin25, 12 LEDs
 np = neopixel.NeoPixel(Pin(25), 12)
 
 # Button on Pin14
-button = Pin(14, Pin.IN)
-state = False
+button = Pin(14, Pin.IN, Pin.PULL_UP)
+button_state = True
 
-# todo: yes this button bounces, thats a problem
-def callback(p):
-    global state
-    if state == False:
-        state = True
-    else:
-        state = False
-    print('pin change', p, state)
+
+def callback(pin):
+    # wait for pin to change value
+    # it needs to be stable for a continuous 20ms
+    global button_state
+    cur_value = pin.value()
+    active = 0
+    while active < 50:
+        if pin.value() != cur_value:
+            active += 1
+        else:
+            active = 0
+    if active > 0:
+        if button_state == False:
+            button_state = True
+        else:
+            button_state = False
 
 
 def clear(np):
@@ -26,7 +36,20 @@ def clear(np):
     np.write()
 
 
-def cycle(np,number,wait=25,color='white',brightness=None,debug=None):
+def disco(np, step=32):
+    n = np.n
+    for i in range(n):
+        for j in range(n):
+            np[j] = tuple((max(0, val - step) for val in np[j]))
+            r = random.randint(0, 255)
+            g = random.randint(0, 255)
+            b = random.randint(0, 255)
+            np[i] = (r, g, b)
+            np.write()
+            time.sleep_ms(50)
+
+
+def cycle(np, number, wait=25, color='white', brightness=None, debug=None):
     n = np.n
     led = number % n
 
@@ -71,11 +94,15 @@ def bounce(np, number, wait = 120):
 
 def demo(np):
     n = np.n
-
     list_com = list(range(n,-1,-2)) + list(range(1,n,2))
     list_com.remove(n)
-
     clear(np)
+
+    disco(np)
+    time.sleep_ms(100)
+    disco(np, step=128)
+    clear(np)
+
     for i in list_com:
         cycle(np, i, 250, color='white', brightness=1)
     time.sleep_ms(100)
@@ -104,28 +131,30 @@ def demo(np):
     # bounce
     for i in range(4 * n):
         bounce(np, i, wait = 120)
-    """
+
     # fade in/out
+    """
     for i in range(0, 4 * 256, 8):
         for j in range(n):
             if (i // 256) % 2 == 0:
                 val = i & 0xff
             else:
                 val = 255 - (i & 0xff)
-            np[j] = (val, 0, 0)
+            np[j] = (val, val, 0)
         np.write()
     """
+    clear(np)
 
-button.irq(trigger=Pin.IRQ_FALLING,handler=callback)
-clear(np)
+button.irq(trigger=Pin.IRQ_FALLING, handler=callback)
 
 try:
     while True:
-        if state == True:
+        if button_state == True:
             demo(np)
-        else:
-            clear(np)
+
 except KeyboardInterrupt:
     print("quit the demo")
+
 finally:
     clear(np)
+
